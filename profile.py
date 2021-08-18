35c16521a6bff28fc7c1fc60a50c146f070f70b9
JoeC75 <54557069+JoeC75@users.noreply.github.com>
2021-08-18 11:48:47 -0400

    changed to 20

--- profile.py
+++ profile.py
@@ -29,7 +29,7 @@ for i in range(num_nodes):
   node.cores = 4
   node.ram = 8192
   node.routable_control_ip = "true" 
-  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
   iface = node.addInterface("if" + str(i))
   iface.component_id = "eth1"
   iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
