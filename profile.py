e01c1fd6a7699973e314dbfc3f656c26d43d4c4c
JoeC75 <54557069+JoeC75@users.noreply.github.com>
2021-08-18 14:33:25 -0400

    Testing Without Kub Setup

--- profile.py
+++ profile.py
@@ -38,12 +38,12 @@ for i in range(num_nodes):
   # setup Docker
   node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
   # setup Kubernetes
-  node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_kubernetes.sh"))
-  node.addService(pg.Execute(shell="sh", command="sudo swapoff -a"))
+  #node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_kubernetes.sh"))
+  #node.addService(pg.Execute(shell="sh", command="sudo swapoff -a"))
   
-  if i == 0:
-    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/kube_manager.sh"))
-  else:
-    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/kube_worker.sh"))
+  #if i == 0:
+    #node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/kube_manager.sh"))
+  #else:
+    #node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/kube_worker.sh"))
     
 pc.printRequestRSpec(request)
